250b2363c88840e316ea30a68d50ec52b2e633a6
Marat Fakhreev <marat.fakhreev@gmail.com>
2023-02-08 19:55:19 +0100

    feat: improve edge cases

--- src/application.ts
+++ src/application.ts
@@ -1,20 +1,36 @@
+const createCounter = (): void => {
+  window.tgCounter = document.createElement('div');
+  window.tgCounter.innerHTML = `
+    <div style="-webkit-font-smoothing: antialiased; text-rendering: optimizeLegibility; font-family: system-ui; border: 1px solid #dfdfdf; border-radius: 12px; position: fixed; background-color: #fff; padding: 12px 18px; top: 18px; right: 18px; width: 180px;">
+      <div style="font-size: 16px;" id="t_media_title"></div>
+      <div style="font-size: 12px;" id="t_media_progress"></div>
+    </div>
+  `;
+
+  document.body.appendChild(window.tgCounter);
+};
+
+const removeCounter = (): void => {
+  setTimeout(() => {
+    window.tgCounter?.remove();
+    delete window.tgCounter;
+  }, 2000);
+};
+
+let titleNode: HTMLElement;
+let progressNode: HTMLElement;
+
 if (window.location.host === 'telegra.ph') {
   if (window.tgCounter) {
-    document.querySelector('#t_media_title').textContent = `Download is interrupted`;
-    setTimeout(() => {
-      window.tgCounter.remove();
-      delete window.tgCounter;
-    }, 2000);
-  } else {
-    window.tgCounter = document.createElement('div');
-    window.tgCounter.innerHTML = `
-        <div style="-webkit-font-smoothing: antialiased; text-rendering: optimizeLegibility; font-family: system-ui; border: 1px solid #dfdfdf; border-radius: 12px; position: fixed; background-color: #fff; padding: 12px 18px; top: 18px; right: 18px; width: 180px;">
-          <div style="font-size: 16px; margin: 0 0 9px;" id="t_media_title"></div>
-          <div style="font-size: 12px;" id="t_media_progress"></div>
-        </div>
-      `;
+    titleNode = document.querySelector('#t_media_title');
+    titleNode.innerHTML = 'Download is interrupted';
+    titleNode.style.margin = '0';
+    progressNode = document.querySelector('#t_media_progress');
+    progressNode.innerHTML = '';
 
-    document.body.appendChild(window.tgCounter);
+    removeCounter();
+  } else {
+    createCounter();
   }
 
   if (window.tgInterval) {
@@ -25,28 +41,43 @@ if (window.location.host === 'telegra.ph') {
     const mediaCount = media.length;
     let i = 0;
 
-    document.querySelector('#t_media_title').innerHTML = `Media count: ${mediaCount}`;
-    window.tgInterval = setInterval(() => {
-      if (mediaCount > i) {
-        const currentAsset: any = media[i];
-        const link = document.createElement('a');
-        const { src } = currentAsset;
+    if (mediaCount === 0) {
+      titleNode = document.querySelector('#t_media_title');
+      titleNode.innerHTML = 'Nothing to download';
+      titleNode.style.margin = '0';
 
-        link.id = i.toString();
-        link.download = src;
-        link.href = src;
-        link.click();
-        ++i;
-        document.querySelector('#t_media_progress').innerHTML = `downloading media: ${i}`;
-      } else {
-        document.querySelector('#t_media_progress').innerHTML = `<b>all media downloaded</b>`;
-        clearInterval(window.tgInterval);
-        setTimeout(() => {
-          window.tgCounter.remove();
-          delete window.tgCounter;
-        }, 2000);
-      }
-    }, 200);
+      clearInterval(window.tgInterval);
+      removeCounter();
+    } else {
+      titleNode = document.querySelector('#t_media_title');
+      titleNode.innerHTML = `Media count: ${mediaCount}`;
+      titleNode.style.margin = '0 0 9px';
+
+      window.tgInterval = setInterval(() => {
+        if (!window.tgCounter) {
+          return;
+        }
+
+        progressNode = document.querySelector('#t_media_progress');
+
+        if (mediaCount > i) {
+          const currentAsset: any = media[i];
+          const link = document.createElement('a');
+          const { src } = currentAsset;
+
+          link.id = i.toString();
+          link.download = src;
+          link.href = src;
+          link.click();
+          progressNode.innerHTML = `downloading media: ${++i}`;
+        } else {
+          progressNode.innerHTML = '<b>all media downloaded</b>';
+
+          clearInterval(window.tgInterval);
+          removeCounter();
+        }
+      }, 200);
+    }
   }
 } else {
   alert('The extension works only on https://telegra.ph website');
